0790f5522e284e03845c771e2b9a7661f8ca56bb
Garbaz <garbaz@t-online.de>
2024-02-19 00:46:02 +0100

    docs example unified

--- src/lib.rs
+++ src/lib.rs
@@ -5,15 +5,17 @@
 //! 
 //! Usage:
 //!
-//! ```
+//! ```rs
 //! use e_write_buffer::WriteBuffer;
-//! use std::fmt::Write;
-//! let mut buffer: WriteBuffer<20> = WriteBuffer::new();
-//! let x = 12;
-//! write!(buffer, "{}", x).unwrap();
-//! assert_eq!(buffer.as_str(), "12");
+//! use std::fmt::Write as _;
+//! 
+//! fn main() {
+//!     let mut buffer: WriteBuffer<20> = WriteBuffer::new();
+//!     let x = 12;
+//!     write!(buffer, "{}", x).unwrap();
+//!     assert_eq!(buffer.as_str(), "12");
+//! }
 //! ```
-
 use core::fmt::{self, Display, Formatter};
 
 /// A write buffer
